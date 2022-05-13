43e267f25cf40a61bd4b8fa0b2673c5629ea82b9
Rick Ertl <ertl.rick@gmail.com>
2022-05-13 13:11:56 -0500

    changed default list to Tasks

--- src/data.js
+++ src/data.js
@@ -3,16 +3,16 @@ import Project from "./project";
 export { projects };
 
 // generate default project
-const project1 = new Project("Project 1", true);
-project1.createTask("Clean room");
-project1.createTask(
+const tasks = new Project("Tasks", true);
+tasks.createTask("Clean room");
+tasks.createTask(
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut turpis massa, tristique eu eleifend in, porta ut tortor. Morbi lorem erat, gravida a bibendum eget, suscipit id orci. Vivamus id felis metus. ",
   "",
   "",
   "low"
 );
-project1.createTask("Empty dishwasher", "", "", "high");
+tasks.createTask("Empty dishwasher", "", "", "high");
 
 // create array of projects and add default project
 const projects = [];
-projects.push(project1);
+projects.push(tasks);
