df4949f6e2ef03ca80f2be16d569fc2ab2b0c052
Rick Ertl <ertl.rick@gmail.com>
2022-05-16 09:15:06 -0500

    switched to sass

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./style.css";
+import "./style.scss";
 import { projects } from "./data.js";
 import { buildProjectView } from "./display.js";
 
--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,20 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        // test: /\.css$/i,
+        // use: ["style-loader", "css-loader"],
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
     ],
   },
 };
+
+//https://dev.to/deepanjangh/setting-up-css-and-sass-with-webpack-3cg
